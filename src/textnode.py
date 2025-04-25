88286c607932fcb8bdde2a53683d2f0e173e2a34
AlessandroKuz <akuzcontact@gmail.com>
2025-04-25 09:43:21 +0200

    feat: add type checks to TextNode and improve existing type hints

--- src/textnode.py
+++ src/textnode.py
@@ -10,12 +10,19 @@ class TextType(Enum):
     IMAGE = 'image'
 
 class TextNode:
-    def __init__(self, text: str, text_type: Enum, url: str = None) -> None:
-        self.text = text
-        self.text_type = text_type
-        self.url = url
+    def __init__(self, text: str, text_type: TextType, url: str = None) -> None:
+        if not isinstance(text, str):
+            raise TypeError('text must be a string')
+        if not isinstance(text_type, TextType):
+            raise TypeError('text_type must be a TextType')
+        if url is not None and not isinstance(url, str):
+            raise TypeError('url must be a string')
+
+        self.text: str = text
+        self.text_type: TextType = text_type
+        self.url: str = url
     
-    def __eq__(self, other: object) -> bool:
+    def __eq__(self, other: "TextNode") -> bool:
         text_is_equal = self.text == other.text
         text_type_is_equal = self.text_type.value == other.text_type.value
         url_is_equal = self.url == other.url
