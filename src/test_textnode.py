dd658838138be0e6cd44483ffd500759b14b7c84
AlessandroKuz <akuzcontact@gmail.com>
2025-04-25 10:14:47 +0200

    refactor(tests): refactor TextNode test to use less functions
    Aggregated some tests to reduce tests footprint.

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -77,26 +77,23 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
- 
+
+        node = TextNode("This is a text node", TextType.ITALIC, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", TextType.ITALIC, "https://www.boot.dev")
+        self.assertEqual(node, node2)
+
     def test_eq_false(self):
         node = TextNode("anchor text", TextType.LINK, 'www.boot.dev')
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(node, node2)
-    
-    def test_eq_false2(self):
+
         node = TextNode("This is a text node", TextType.TEXT)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(node, node2)
 
-    def test_eq_false3(self):
         node = TextNode("This is a text node", TextType.TEXT)
         node2 = TextNode("This is a text node2", TextType.TEXT)
         self.assertNotEqual(node, node2)
-
-    def test_eq_url(self):
-        node = TextNode("This is a text node", TextType.ITALIC, "https://www.boot.dev")
-        node2 = TextNode("This is a text node", TextType.ITALIC, "https://www.boot.dev")
-        self.assertEqual(node, node2)
 
     def test_repr(self):
         text = "This is a text node"
