e73592a15929a39929614ef9608268753c272de6
AlessandroKuz <akuzcontact@gmail.com>
2025-04-25 10:24:44 +0200

    feat: add HTMLNode class and define basic attributes and methods

---
+++ src/htmlnode.py
@@ -0,0 +1,32 @@
+class HTMLNode:
+    def __init__(self, tag: str = None, value: str = None, children: list["HTMLNode"] = None, props: dict[str, str] = None) -> None:
+        if tag is not None and not isinstance(tag, str):
+            raise TypeError("tag must be a string")
+        if value is not None and not isinstance(value, str):
+            raise TypeError("value must be a string")
+        if children is not None and not isinstance(children, list):
+            raise TypeError("children must be a list")
+        if props is not None and not isinstance(props, dict):
+            raise TypeError("props must be a dictionary")
+
+        self.tag: str = tag
+        self.value: str = value
+        self.children: list["HTMLNode"] = children
+        self.props: dict[str, str] = props
+
+    def to_html(self) -> str:
+        raise NotImplementedError()
+
+    def props_to_html(self) -> str:
+        attrs_string = ''.join([f' {key}="{val}"' for key, val in self.props.items()])
+        return attrs_string
+
+    def __eq__(self, other: "HTMLNode") -> bool:
+        tag_is_equal = self.tag == other.tag
+        value_is_equal = self.value == other.value
+        children_is_equal = self.children == other.children
+        props_is_equal = self.props == other.props
+        return tag_is_equal and value_is_equal and children_is_equal and props_is_equal
+
+    def __repr__(self) -> str:
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
