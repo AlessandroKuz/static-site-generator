46dd44b34690f62fb91fb2254897539e2eb1e92c
AlessandroKuz <akuzcontact@gmail.com>
2025-04-29 23:42:01 +0200

    fix(LeafNode): fix value check and add missing repr method

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -40,11 +40,17 @@ class HTMLNode:
 
 class LeafNode(HTMLNode):
     def __init__(self, tag: str | None, value: str, props: dict[str, str] | None = None) -> None:
+        if tag == '':
+            tag = None
         super().__init__(tag, value, None, props)
 
     def to_html(self) -> str:
-        if not self.value:
+        if self.value is None:
             raise ValueError("value can't be empty. All leaf nodes must have a value.")
         if self.tag is None:
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
