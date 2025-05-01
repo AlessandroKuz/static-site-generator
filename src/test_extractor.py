af97f9085e11b0bb2f62516d6767280842221d90
AlessandroKuz <akuzcontact@gmail.com>
2025-05-01 13:18:32 +0200

    test: add tests for the images and link extractor functions

---
+++ src/test_extractor.py
@@ -0,0 +1,21 @@
+import unittest
+
+from extractor import extract_markdown_images, extract_markdown_links
+
+
+class TestExtractMarkdownFunctions(unittest.TestCase):
+    def test_images_extraction(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        images = extract_markdown_images(text)
+        expected_result = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        self.assertEqual(expected_result, images)
+
+    def test_links_extraction(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        links = extract_markdown_links(text)
+        expected_result = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+        self.assertEqual(expected_result, links)
+
+
+    if __name__ == '__main__':
+        unittest.main()
