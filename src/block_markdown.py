794290a47d7d1df48de74814168bcad64248a34f
Alessandro Kuz <akuzcontact@gmail.com>
2025-05-02 12:16:01 +0000

    feat: add improvements to md to blocks function

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -2,7 +2,8 @@ import re
 
 
 def markdown_to_blocks(markdown: str) -> list[str]:
-    normalized_markdown = re.sub(r'\n{2,}', '\n\n', markdown)
+    normalized_markdown = re.sub(r'[\t ]+\n', '\n', markdown)
+    normalized_markdown = re.sub(r'\n{2,}', '\n\n', normalized_markdown)
     blocks: list[str] = normalized_markdown.split('\n\n')
-    blocks = [block.strip() for block in blocks if block]
+    blocks = [block.strip() for block in blocks if block.strip()]
     return blocks
