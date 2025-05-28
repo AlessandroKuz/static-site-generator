34581206942eb4f2b9a1b252b36fd6ddb3129f0a
AlessandroKuz <akuzcontact@gmail.com>
2025-05-28 11:38:38 +0200

    feat: add function to generate multiple pages from a dir

--- src/generate_html.py
+++ src/generate_html.py
@@ -37,7 +37,7 @@ def generate_page(
         raise FileNotFoundError(f"{template_path.resolve()} does not exist")
 
     if not dest_path.parent.exists():
-        raise ValueError(f"{dest_path.parent.resolve()} does not exist")
+        dest_path.parent.mkdir(parents=True, exist_ok=True)
 
     if not from_path.is_file():
         raise ValueError(f"{from_path.resolve()} is not a file")
@@ -77,3 +77,62 @@ def generate_page(
         html_file.write(template)
 
     print(f"Page generated successfully to {dest_path.resolve()}!")
+
+def generate_pages_recursive(
+        dir_path_content: pathlib.Path | str,
+        template_path: pathlib.Path | str,
+        dest_dir_path: pathlib.Path | str) -> None:
+    """
+
+    :param dir_path_content: the path of the directory containing the Markdown files.
+    :param template_path: the path to the HTML template to use.
+    :param dest_dir_path: the path to the output HTML files.
+
+    :return: None
+    """
+    if not (isinstance(dir_path_content, pathlib.Path) or isinstance(dir_path_content, str)):
+        raise TypeError(f"dir_path_content must be a pathlib.Path or str; instead got: {type(dir_path_content)}")
+    if not (isinstance(template_path, pathlib.Path) or isinstance(template_path, str)):
+        raise TypeError(f"template_path must be a pathlib.Path or str; instead got: {type(template_path)}")
+    if not (isinstance(dest_dir_path, pathlib.Path) or isinstance(dest_dir_path, str)):
+        raise TypeError(f"dest_dir_path must be a pathlib.Path or str; instead got: {type(dest_dir_path)}")
+
+    if isinstance(dir_path_content, str):
+        dir_path_content = pathlib.Path(dir_path_content)
+    if isinstance(template_path, str):
+        template_path = pathlib.Path(template_path)
+    if isinstance(dest_dir_path, str):
+        dest_dir_path = pathlib.Path(dest_dir_path)
+
+    # Check is provided paths exist and are valid
+    if not dir_path_content.exists():
+        raise FileNotFoundError(f"{dir_path_content.resolve()} does not exist")
+    if not template_path.exists():
+        raise FileNotFoundError(f"{template_path.resolve()} does not exist")
+
+    if not dest_dir_path.exists():
+        dest_dir_path.mkdir(parents=True, exist_ok=True)
+
+    if dir_path_content.is_file():
+        raise ValueError(f"{dir_path_content.resolve()} is a file")
+    if not template_path.is_file():
+        raise ValueError(f"{template_path.resolve()} is not a file")
+
+    SUPPORTED_HTML_EXTENSIONS = ['.html', '.htm']
+    if template_path.suffix not in SUPPORTED_HTML_EXTENSIONS:
+        raise ValueError(
+            f"{template_path.suffix} is not an HTML file; supported extensions: {','.join(SUPPORTED_HTML_EXTENSIONS)}"
+        )
+
+    # Start generating the HTML page
+    print(f"Generating all pages from {dir_path_content} to {dest_dir_path} using {template_path}")
+
+    for item in dir_path_content.iterdir():
+        if item.is_file():
+            dest_path = f"{dest_dir_path}/{item.stem}{template_path.suffix}"
+            generate_page(item, template_path, dest_path)
+        else:
+            new_dest_dir_path = dest_dir_path / item.name
+            generate_pages_recursive(item, template_path, new_dest_dir_path)
+
+    print(f"All pages generated successfully to {dest_dir_path.resolve()}!")
--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
 from copystatic import copy_files_recursively
-from generate_html import generate_page
+from generate_html import generate_pages_recursive
 
 
 def main():
-    src = 'static'
-    dst = 'public'
+    src = "static"
+    dst = "public"
     verbose = False
     copy_files_recursively(src, dst, verbose=verbose)
-    from_path = "content/index.md"
+    from_path = "content"
     template_path = "template.html"
-    dest_path = "public/index.html"
-    generate_page(from_path, template_path, dest_path)
+    dest_path = "public"
+    generate_pages_recursive(from_path, template_path, dest_path)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
